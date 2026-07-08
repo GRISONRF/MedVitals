e0bcd5a347c1e3d373d69273cdf89900aa4ed6d2
rafaela <grisonrf@gmail.com>
2026-07-08 16:49:19 -0300

    First version of this app, up and running

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,20 +1,36 @@
-export interface HumanName {
-  given: string[];
+export interface FHIRIdentifier {
+  use: string;
+  system: string;
+  value: string;
+}
+
+export interface FHIRName {
+  use: string;
   family: string;
+  given: string[];
 }
 
 export interface FHIRPatient {
-  resourceType: string;
+  resourceType: "Patient";
   id: string;
-  name: HumanName[];
+  active: boolean;
+  name: FHIRName[];
   gender: string;
   birthDate: string;
+  identifier: FHIRIdentifier[]; // To store things like NPI or Medical Record Numbers
 }
 
 export interface VitalObservation {
-  resourceType: string;
+  resourceType: "Observation";
   id: string;
-  code: string;
-  value: string;
+  status: string;
+  code: {
+    text: string;
+    coding: Array<{ system: string; code: string; display: string }>;
+  };
+  valueQuantity: {
+    value: number;
+    unit: string;
+  };
   effectiveDateTime: string;
 }
